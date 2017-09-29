98351eb249f81e6f8be434af91e4dd9fd3569fd2
Catherine <cmlavin7@gmail.com>
2017-09-29 12:34:54 -0400

    Added video backgrounds

---
+++ src/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Title = () => {
+  return(
+    <div id="title-text">
+      <img src="clipart-apple.png" alt="apple" width="4%"/>
+      Big Apple Dwellers
+    </div>
+  )
+}
+
+export default Title
---
+++ src/components/Homepage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import VideoBkgdWelcome from './VideoBkgdWelcome'
+import Title from '../Title'
+import Welcome from './Welcome'
+
+const Homepage = () => {
+  return(
+    <div>
+      <VideoBkgdWelcome/>
+      <Title />
+      <Welcome />
+    </div>
+  )
+}
+
+export default Homepage
